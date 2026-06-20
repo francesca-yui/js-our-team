1ab5497bc1d9f4e61052fc2184c30150e08beb1e
Francesca Yu <yui.francesca@gmail.com>
2026-06-20 12:06:36 +0200

    set html js css

--- script.js
+++ script.js
@@ -36,3 +36,88 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+const teamContainerDiv = document.querySelector('.team-container');
+const newMemberForm = document.querySelector('#new-member');
+
+const memberForm = {
+  nameInput: newMemberForm.querySelector ('#name'),
+  roleInput: newMemberForm.querySelector ('#role'),
+  emailInput: newMemberForm.querySelector ('#email'),
+  imgInput: newMemberForm.querySelector ('#img')
+};
+
+
+const teamMemberCardTemplate = ({name, role, email, img}) => `
+  <div class="team-card">
+    <div class="card-image">
+      <img src="${img}" alt="${name}"/>
+    </div>
+    <div class="card-text">
+      <h3 class="name">${name}</h3>
+      <p class="role">${role}</p>
+      <p class="email">${email}</p> 
+    </div>
+    <button id="${name}">Delete</button>
+  </div>
+`;
+
+const renderTeam = () => {
+  let html = '';
+  for (let i = 0; i < teamMembers.length; i++) {
+    const member = teamMembers[i];
+    html += teamMemberCardTemplate(member);
+  }
+
+  teamContainerDiv.innerHTML = html;
+
+  const buttons = teamContainerDiv.querySelectorAll('button');
+
+  for (const button of buttons) {
+    button.addEventListener('click', () => {
+      const { id: memberName } = button;
+
+      let index = -1;
+      for (i = 0; i < teamMembers.length; i++) {
+        if (teamMembers[i].name === memberName) index = i;
+      }
+      
+      if (index === -1) return;
+
+      teamMembers.splice(index, 1);   
+
+      renderTeam();
+    });
+  }
+};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const name = memberForm.nameInput.value;
+  const role = memberForm.roleInput.value;
+  const email = memberForm.emailInput.value;
+  const img = memberForm.imgInput.value;
+
+  teamMembers.push({name, role, email, img});
+
+  renderTeam();
+};
+
+newMemberForm.addEventListener('submit', onFormSubmit);
+
+renderTeam();
+
+
+/*
+<div class="team-card">
+ <div class="card-image">
+  <img src="img/male1.png" alt="Marco Bianchi">
+  </div>
+  <div class="card-text">
+    <h3 class="name">Marco Bianchi</h3>
+    <p class="role">Designer</p>
+    <p class="email">marcobianchi@team.com</p>    
+  </div>   
+</div> 
+*/
